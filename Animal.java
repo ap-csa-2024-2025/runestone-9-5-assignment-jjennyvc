df6da4e6e5ac6841a71789bfa95f624f26ae13b9
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2025-04-07 18:18:32 +0000

    jj

--- Animal.java
+++ Animal.java
@@ -1,4 +1,17 @@
 public class Animal
 {
-    
+    private String name;
+    private String species;
+    private int age;
+
+    public class Animals(String name, String species, int age)
+    {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+    }
+    public void matingCall()
+    {
+        System.out.println("");
+    }
 }
